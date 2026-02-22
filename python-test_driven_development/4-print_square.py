782181e2eb2b2ee69360dbe9613ac085aec12764
CahangirQU <gence2017m@gmail.com>
2026-02-22 18:38:13 +0000

    sikma

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that defines a function to print a square."""
+
+
+def print_square(size):
+    """Prints a square using the character '#'."""
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
