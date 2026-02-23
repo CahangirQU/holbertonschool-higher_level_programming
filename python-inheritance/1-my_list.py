8873a1b7bf55348d65580984fcea424df84f15f1
CahangirQU <gence2017m@gmail.com>
2026-02-23 21:16:15 +0000

    ders1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,7 +2,7 @@
 """Module 1-my_list: defines MyList class"""
 
 class MyList(list):
-    """MyList inherits from list and has a print_sorted method"""
+    """MyList inherits from list and adds print_sorted method"""
     def print_sorted(self):
         """Prints the list in ascending order without modifying original"""
         print(sorted(self))
