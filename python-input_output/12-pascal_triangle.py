3eb019c069ff820c64665ddf1882ef1b987a89e8
CahangirQU <gence2017m@gmail.com>
2026-03-03 22:46:02 +0000

    ders0

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""pascal triangle."""
+
+
+def pascal_triangle(n):
+    """Generate the first n rows of Pascal's triangle."""
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle
