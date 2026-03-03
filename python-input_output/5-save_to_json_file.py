ce25f2cbf03b06202db2021cb9fe02778965b867
CahangirQU <gence2017m@gmail.com>
2026-03-03 22:42:51 +0000

    ders0

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module change strind and write file"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Returns the change of the string and write file"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
