56c060af8f3037185e313d9f70ffbea509cc72e0
CahangirQU <gence2017m@gmail.com>
2026-04-05 18:51:22 +0000

    ders0

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,14 +1,23 @@
-from flask import Flask,  render_template
 import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/items")
-def home():
-    with open("items.json", "r") as f:
-        data = json.load(f)
-    items = data.get("items", [])
-    return render_template("items.html", items=items)
+@app.route('/items')
+def items_list():
+    try:
+        with open('items.json', 'r') as f:
+            data = json.load(f)
+            items = data.get("items", [])
+    except FileNotFoundError:
+        items = []
+        
+    return render_template('items.html', items=items)
 
-if __name__ == "__main__":
-    app.run(debug=True,port=5000)
+# Əvvəlki tapşırıqdan olan marşrutları da əlavə edək (istəyə bağlı)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
